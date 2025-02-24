8876c8a9bc81fe7927e3317e605f42df6e9e12c4
PeterS-Kang <kang510@purdue.edu>
2025-02-24 09:50:35 -0500

    slight changes to _app and index

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -7,12 +7,15 @@ import Sidebar from '../components/Sidebar/Sidebar'
 
 export default function Home()  {
   return (
-    <div className='bg-[#1E252B] w-screen min-h-screen'>
-        <Navbar/>
-        <div className='flex flex-row'>
-            <Sidebar/>
-            <Forum/>
-        </div>
-    </div>
+    // <div className='bg-[#1E252B] w-screen min-h-screen'>
+    //     <Navbar/>
+    //     <div className='flex flex-row'>
+    //         <Sidebar/>
+    //         <Forum/>
+    //     </div>
+    // </div>
+    <>
+      <Forum/>
+    </>
   )
 }
