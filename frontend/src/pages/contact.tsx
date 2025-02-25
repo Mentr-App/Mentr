71d58f4b92c167590588449eb29abcb24bd10167
Matthew Sigit <message4mas@gmail.com>
2025-02-25 17:07:30 -0500

    CONTACT SCUFFED BUT WORKS

---
+++ frontend/src/pages/contact.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Contact from "@/components/Contact/Contact";
+
+const ContactPage: React.FC = () => {
+    return (
+        <div className="container mx-auto px-4 py-8">
+            <Contact/>
+        </div>
+    );
+};
+
+export default ContactPage;
