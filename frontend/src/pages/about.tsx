d93957dfd4451b6ed86fb2cd7784f80fa585c056
Matthew Sigit <message4mas@gmail.com>
2025-02-25 18:00:25 -0500

    updated dropdown for navbar, added filler pages to about and pp

---
+++ frontend/src/pages/about.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import About from "@/components/About/About";
+
+const AboutPage: React.FC = () => {
+    return (
+        <div className="container mx-auto px-4 py-8">
+            <About/>
+        </div>
+    );
+};
+
+export default AboutPage;
