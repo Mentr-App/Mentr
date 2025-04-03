4a9a892ff5623d95c8b21b68d0b4e7974f41e0f1
PeterS-Kang <kang510@purdue.edu>
2025-04-03 19:12:19 -0400

    Fixed sort i tink so

--- frontend/src/pages/api/feed.ts
+++ frontend/src/pages/api/feed.ts
@@ -32,6 +32,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         }
 
         const data = await response.json();
+        console.log(data)
         // console.log(data);
         return res.status(200).json(data);
     } catch (error) {
